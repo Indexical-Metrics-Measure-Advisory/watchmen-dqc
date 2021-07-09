276da1f2df3ff78ccef5860a4fe4f3da3b6870a0
luke0623 <luke0623@outlook.com>
2021-07-09 11:19:05 +0800

    add factor rule

--- dqc/router/analysis.py
+++ dqc/router/analysis.py
@@ -5,7 +5,7 @@ from pydantic.main import BaseModel
 
 from dqc.model.analysis.monitor_rule import MonitorRule
 from dqc.service.analysis.analysis_service import load_global_rule_list, load_topic_rule_list_by_topic_id, \
-    create_monitor_rule, update_monitor_rule
+    create_monitor_rule, update_monitor_rule, load_monitor_rule
 
 router = APIRouter()
 
@@ -18,38 +18,14 @@ class MonitorRulesCriteria(BaseModel):
 class MonitorRuleRequest(BaseModel):
     criteria: MonitorRulesCriteria
 
-#
-# ## add rules to topic
-# @router.post("/dqc/topic/rules", tags=["admin"], response_model=List[TopicRule])
-# async def add_rule_to_topic(topic_rule_list: List[TopicRule]):
-#     for topic_rule in topic_rule_list:
-#         topic_rule.topicRuleId = get_next_id()
-#     return insert_all(topic_rule_list, TopicRule, TOPIC_RULES)
-#
-#
-# ## add rules to factor
-# @router.post("/dqc/factor/rules", tags=["admin"], response_model=List[FactorRule])
-# async def add_rule_to_factor(factor_rule_list: List[FactorRule]):
-#     for factor_rule in factor_rule_list:
-#         factor_rule.factorRuleId = get_next_id()
-#     return insert_all(factor_rule_list, FactorRule, FACTOR_RULES)
-#
-#
-# @router.post("/dqc/topic/rule/update", tags=["admin"], response_model=TopicRule)
-# async def update_topic_rule(topic_rule: TopicRule):
-#     return update_one_with_key(topic_rule, TopicRule, TOPIC_RULES, "topicRuleId")
-#
-#
-# @router.post("/dqc/factor/rule/update", tags=["admin"], response_model=FactorRule)
-# async def update_factor_rule(factor_rule: FactorRule):
-#     return update_one_with_key(factor_rule, FactorRule, FACTOR_RULES, "factorRuleId")
-#
+
 
 
 @router.post("/dqc/monitor/rules", tags=["admin"], response_model=List[MonitorRule])
 async def save_monitor_rule(rule_list: List[MonitorRule]):
     for monitor_rule in rule_list:
-        if monitor_rule.uid is None:
+        result = load_monitor_rule(monitor_rule)
+        if result is None:
             create_monitor_rule(monitor_rule)
         else:
             update_monitor_rule(monitor_rule)
--- dqc/service/analysis/analysis_service.py
+++ dqc/service/analysis/analysis_service.py
@@ -1,5 +1,5 @@
 from storage.mongo.mongo_new_template import update_one_with_key
-from storage.storage.storage_template import find_, insert_one
+from storage.storage.storage_template import find_, insert_one, update_one, update_, find_one
 
 from dqc.common.simpleflake import get_next_id
 from dqc.model.analysis.monitor_rule import MonitorRule
@@ -15,10 +15,26 @@ def load_topic_rule_list_by_topic_id(topic_id):
     return find_({"grade": "topic", "topicId": topic_id}, MonitorRule, MONITOR_RULES)
 
 
+def load_monitor_rule(monitor_rule):
+    if monitor_rule.factorId is None:
+        return find_one({"and": [{"topicId": monitor_rule.topicId}, {"code": monitor_rule.code}]},
+                       MonitorRule, MONITOR_RULES)
+    else:
+        return find_one({"and": [{"topicId": monitor_rule.topicId}, {"code": monitor_rule.code},
+                                {"factorId": monitor_rule.factorId}]},
+                       MonitorRule, MONITOR_RULES)
+
+
 def create_monitor_rule(monitor_rule: MonitorRule):
     monitor_rule.uid = get_next_id()
     return insert_one(monitor_rule, MonitorRule, MONITOR_RULES)
 
 
 def update_monitor_rule(monitor_rule: MonitorRule):
-    return update_one_with_key(monitor_rule, MonitorRule, MONITOR_RULES, "uid")
+    if monitor_rule.factorId is None:
+        print("topic ")
+        return update_({"and":[{"topicId":monitor_rule.topicId},{"code":monitor_rule.code}]},monitor_rule, MonitorRule, MONITOR_RULES)
+    else:
+        print("factor ")
+        return update_({"and": [{"topicId": monitor_rule.topicId}, {"code": monitor_rule.code},{"factorId":monitor_rule.factorId}]}, monitor_rule,
+                       MonitorRule, MONITOR_RULES)
