c95109f4f17df296afbddedabb7ea9304ce67a7a
luke0623 <luke0623@outlook.com>
2021-07-29 18:28:38 +0800

    Revert "update"
    This reverts commit 3d3929ecc779b885c695858417b6a1d029fb9622.

--- dqc/router/analysis.py
+++ dqc/router/analysis.py
@@ -1,20 +1,16 @@
-import json
-from datetime import datetime
+import datetime
 from typing import List
 
-import arrow
 from fastapi import APIRouter, Depends
 from pydantic.main import BaseModel
 
 from dqc.common import deps
 from dqc.common.simpleflake import get_next_id
-from dqc.common.utils.data_utils import get_date_range_with_end_date
 from dqc.model.analysis.monitor_rule import MonitorRule
 from dqc.model.analysis.monitor_rule_log import MonitorRuleLog
 from dqc.model.analysis.rule_result_criteria import MonitorRuleLogRequest
-from dqc.sdk.admin.admin_sdk import get_topic_by_id
 from dqc.service.analysis.analysis_service import load_global_rule_list, load_topic_rule_list_by_topic_id, \
-    create_monitor_rule, update_monitor_rule, load_monitor_rule, topic_profile
+    create_monitor_rule, update_monitor_rule, load_monitor_rule
 from dqc.service.query.index import query_rule_results_by_datetime
 
 router = APIRouter()
@@ -32,11 +28,11 @@ class MonitorRuleRequest(BaseModel):
 class TopicProfileRequest(BaseModel):
     topicId: str = None
     fromDate: datetime = None
-    toDate: datetime = None
+    toDate:datetime = None
 
 
 @router.post("/dqc/monitor/rules", tags=["admin"], response_model=List[MonitorRule])
-async def save_monitor_rule(rule_list: List[MonitorRule], current_user=Depends(deps.get_current_user)):
+async def save_monitor_rule(rule_list: List[MonitorRule],current_user = Depends(deps.get_current_user)):
     for monitor_rule in rule_list:
         if monitor_rule.ruleId is None:
             monitor_rule.ruleId = get_next_id()
@@ -49,7 +45,7 @@ async def save_monitor_rule(rule_list: List[MonitorRule], current_user=Depends(d
 
 
 @router.post("/dqc/monitor/query", tags=["admin"], response_model=List[MonitorRule])
-async def query_monitor_rules(req: MonitorRuleRequest, current_user=Depends(deps.get_current_user)):
+async def query_monitor_rules(req: MonitorRuleRequest,current_user = Depends(deps.get_current_user)):
     criteria = req.criteria
     if criteria.grade == "global":
         return load_global_rule_list()
@@ -58,16 +54,11 @@ async def query_monitor_rules(req: MonitorRuleRequest, current_user=Depends(deps
 
 
 @router.post("/dqc/rule/result/query", tags=["admin"], response_model=List[MonitorRuleLog])
-async def query_rule_results(req: MonitorRuleLogRequest, current_user=Depends(deps.get_current_user)):
+async def query_rule_results(req: MonitorRuleLogRequest,current_user = Depends(deps.get_current_user)):
     results = query_rule_results_by_datetime(req.criteria)
     # print (results)
     return results
 
 
-@router.get("/dqc/topic/profile", tags=["analytics"])
-def generate_topic_profile(topic_id: str, date: str, current_user=Depends(deps.get_current_user)):
-    query_date = arrow.get(date)
-    topic = get_topic_by_id(topic_id)
-    from_, to_ = get_date_range_with_end_date("daily", query_date)
-    data = topic_profile(topic, from_, to_)
-    return json.loads(data)
+async def generate_topic_profile():
+    pass
